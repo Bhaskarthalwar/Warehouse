078f5452d9cd218810031f57c92cf6a5ae5d47b5
bhaskar thalwar <bhaskarthalwar@gmail.com>
2022-03-09 23:04:57 +0530

    changed the put method to patch for product sell

--- src/main/java/com/warehouse/application/controllers/ProductController.java
+++ src/main/java/com/warehouse/application/controllers/ProductController.java
@@ -12,11 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * API endpoints for Product setup
@@ -61,7 +57,7 @@ public class ProductController {
      * @param artId
      * @return Updated Product and Inventory or Warehouse as a whole
      */
-    @PutMapping(value = "/warehouse/product/{name}/{artId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PatchMapping(value = "/warehouse/product/{name}/{artId}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<Void> sellProduct(@PathVariable String name, @PathVariable int artId) {
         prodManager.sellProduct(name, artId);
